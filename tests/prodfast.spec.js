1445f682082b7ae8b8ec265d90e8a78de97a7496
SSugar <Sugar>
2024-01-16 18:21:08 +0200

    commit push add6

--- tests/prodfast.spec.js
+++ tests/prodfast.spec.js
@@ -3,11 +3,11 @@ const { test, expect } = require('@playwright/test')
 test('Prod check fast @prod', async ({ page }) => {
 
     //await page.goto('https://portal.ringrx.com/login')
-    test.setTimeout(100000);
+    //test.setTimeout(10000);
     //await page.pause();
     await page.goto('https://portal.ringrx.com/login');
     await page.fill('[placeholder="Enter Login"]', 'test');
-    await page.fill('[placeholddddd="kfkfkf"]', 'test');
+    await page.fill('[placeholder="kfkfkf"]', 'test');
     await page.click('button', { name: "Sig in" });
     //await page.waitForSelector('');
     await page.getByTitle('Dismiss this notification').locator('path').click();
