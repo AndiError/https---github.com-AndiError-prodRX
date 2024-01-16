dfb63b7d7f998063602003b2522b603728e4e59c
SSugar <Sugar>
2024-01-16 17:23:01 +0200

    new test prod check

---
+++ tests/prodfast.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test')
+
+test('Prod check fast @prod', async ({ page }) => {
+
+    await page.goto('https://portal.ringrx.com/login')
+    test.setTimeout(100000);
+    //await page.pause();
+    await page.goto('https://portal.ringrx.com/login');
+    await page.fill('[placeholder="Enter Login"]', 'test');
+    await page.fill('[placeholder="Enter Password"]', 'test');
+    await page.click('button', { name: "Sign in" });
+    //await page.waitForSelector('');
+    await page.getByTitle('Dismiss this notification').locator('path').click();
+
+})
