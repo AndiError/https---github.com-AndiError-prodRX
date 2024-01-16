2787f43c65da4c802b4f92e451c1cefa80f964de
SSugar <Sugar>
2024-01-16 21:58:19 +0200

    message ghangesqqq

--- tests/prodfast.spec.js
+++ tests/prodfast.spec.js
@@ -4,11 +4,11 @@ test('Prod check fast @prod', async ({ page }) => {
 
     //await page.goto('https://portal.ringrx.com/login')
     //test.setTimeout(10000);
-    //await page.pause();
+    await page.pause();
     await page.goto('https://portal.ringrx.com/login');
     await page.fill('[placeholder="Enter Login"]', 'test');
-    await page.fill('[placeholder="kfkfkf"]', 'test');
-    await page.click('button', { name: "Sig in" });
+    await page.fill('[placeholder="Enter Password"]', 'test');
+    await page.click('button', { name: "Sign in" });
     //await page.waitForSelector('');
     await page.getByTitle('Dismiss this notification').locator('path').click();
 
